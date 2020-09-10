b494a8ee1eab6836af590f51873ef7f5e82f34a1
quietsamurai98 <quietsamurai98@users.noreply.github.com>
2020-09-09 19:18:50 -0500

    Got (almost!) purely functional level generation working for normal rooms.

--- app/dungeon_master.rb
+++ app/dungeon_master.rb
@@ -3,7 +3,7 @@ module DungeonMaster
   def DungeonMaster::generate(params)
     initial_layout = DungeonMaster::add_room!({}, DungeonMaster::init_room(0, 0, :spawn, 1))
     prng           = PRNG.new(params[:config][:seed])
-    layout         = (1..params[:room_counts][:normal]).reduce(initial_layout) { |acc, _| DungeonMaster::add_normal_room!(acc, 1, prng) }
+    layout, _      = (1..params[:room_counts][:normal]).reduce([initial_layout, [[0, 0]]]) { |acc, _| DungeonMaster::add_normal_room!(acc[0], acc[1], 1, prng) }
     puts DungeonMaster::layout_str(layout)
   end
 
@@ -44,6 +44,7 @@ module DungeonMaster
 
   # @param [Hash] layout
   # @param [Hash] room
+  # TODO: CURE SIDE EFFECT! Currently modifies layout parameter.
   def DungeonMaster::add_room!(layout, room)
     layout["#{(room[:x] >= 0 ? '+' : '')}#{room[:x]}#{(room[:y] >= 0 ? '+' : '')}#{room[:y]}".to_sym] = room
     layout
@@ -59,45 +60,34 @@ module DungeonMaster
   # @param [Hash] layout
   # @param [Integer] x
   # @param [Integer] y
-  def DungeonMaster::forms_2x2?(layout, x, y)
-    north = DungeonMaster::get_room(layout, x, y + 1) != nil
-    south = DungeonMaster::get_room(layout, x, y - 1) != nil
-    east  = DungeonMaster::get_room(layout, x + 1, y) != nil
-    west  = DungeonMaster::get_room(layout, x - 1, y) != nil
-
-    (((DungeonMaster::get_room(layout, x + 1, y + 1) != nil) && north && east) ||
-        ((DungeonMaster::get_room(layout, x - 1, y + 1) != nil) && north && west) ||
-        ((DungeonMaster::get_room(layout, x + 1, y - 1) != nil) && south && east) ||
-        ((DungeonMaster::get_room(layout, x - 1, y - 1) != nil) && south && west))
+  def DungeonMaster::valid_children(layout, x, y)
+    child_coords = [[1, 0], [-1, 0], [0, 1], [0, -1]].map { |xy| [xy[0] + x, xy[1] + y] }
+    child_coords.find_all { |xy| DungeonMaster::neighbors(layout, *xy).length == 1 }
   end
 
   # @param [Hash] layout
+  # @param [Array] working_set
   # @param [Integer] stage
   # @param [Hash] config
   # @param [PRNG] prng
-  def DungeonMaster::add_normal_room!(layout, stage, prng)
-    min_x, max_x = *(layout.values.reduce([999999, -999999]) { |acc, val| val[:stage] == stage ? [acc[0].lesser(val.x), acc[1].greater(val.x)] : acc })
-    min_y, max_y = *(layout.values.reduce([999999, -999999]) { |acc, val| val[:stage] == stage ? [acc[0].lesser(val.y), acc[1].greater(val.y)] : acc })
-    valid_coords = (min_x - 1..max_x + 1).to_a.product((min_y - 1..max_y + 1).to_a).find_all do |xy|
-      if DungeonMaster::get_room(layout, *xy) != nil || DungeonMaster::forms_2x2?(layout, *xy)
-        false
-      else
-        neighbor_list = DungeonMaster::neighbors(layout, *xy)
-        (!neighbor_list.empty?) && neighbor_list.all? do |room|
-          ([:normal, :spawn].include?(room[:type]) && room[:stage] == stage) || (room[:type] == :boss && room[:stage] == stage - 1)
-        end
-      end
+  # Has side effects due to add_room!
+  def DungeonMaster::add_normal_room!(layout, working_set, stage, prng)
+    parent   = prng.sample working_set
+    children = DungeonMaster::valid_children(layout, *parent)
+    if children.empty?
+      return DungeonMaster::add_normal_room!(layout, working_set - [parent], stage, prng)
+    else
+      child      = prng.sample children
+      new_layout = DungeonMaster::add_room!(layout, DungeonMaster::init_room(*child, :normal, stage))
+      return [new_layout, working_set + [child]]
     end
-    room_coords  = prng.sample(valid_coords)
-    add_room!(layout, DungeonMaster::init_room(*room_coords, :normal, stage))
-    layout
   end
 
   # @param [Hash] layout
   def DungeonMaster::layout_str(layout)
     min_x, max_x = *(layout.values.reduce([999999, -999999]) { |acc, val| [acc[0].lesser(val.x), acc[1].greater(val.x)] })
     min_y, max_y = *(layout.values.reduce([999999, -999999]) { |acc, val| [acc[0].lesser(val.y), acc[1].greater(val.y)] })
-    char_arr     = (min_y..max_y + 1).to_a.map { |_| (" " * (max_x + 1 - min_x)).chars }
+    char_arr     = (min_y..max_y).to_a.map { |_| (" " * (max_x + 1 - min_x)).chars }
     layout.values.each do |room|
       char_arr[room[:y] - min_y][room[:x] - min_x] = room[:type].to_s[0]
     end
