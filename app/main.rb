e18a175db2b65238b3a2e65f69b899f8cdcef7dd
quietsamurai98 <quietsamurai98@users.noreply.github.com>
2020-09-11 17:16:47 -0500

    Modified the comment in main.rb

--- app/main.rb
+++ app/main.rb
@@ -5,9 +5,16 @@ require 'app/bullet.rb'
 require 'app/controller.rb'
 require 'lib/xy_vector.rb'
 
-# <name of sample app> is a twinstick prototype designed to be 'stateless'
-# Stateless in this sense describes the usage of modular functions that do
-# not depend on a state to give correct output.
+# <name of sample app> is a prototype of a top-down twin-stick shooter
+# similar to Smash TV or The Binding of Issac, designed to be 'stateless'.
+# Stateless in this sense refers to the fact that all the defined methods
+# are pure functions that do not modify their input parameters, while
+# producing the same output given the same input. Rather than using class
+# objects, functions use Hashes as universal inputs and outputs.
+# Functions that operate on similar entities, such as bullets, the player
+# character, etc. are grouped into modules, which are broken out into
+# separate files for easier organization.
+#
 # The inspiration for designing the code base in this way comes from two
 # presentations:
 #
