eb632c9358a5e53615c78a3a534d68df521b36d1
quietsamurai98 <quietsamurai98@users.noreply.github.com>
2020-09-25 15:32:08 -0500

    added Zif tick trace service.

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,6 @@
+require 'lib/zif/services/tick_trace_service.rb'
+$trace_service = Zif::TickTraceService.new(0.03)
+
 require 'lib/deepmerge.rb'
 require 'lib/xy_vector.rb'
 require 'lib/prng.rb'
@@ -16,6 +19,8 @@ $last_time  = Time.now.usec
 
 # @param [GTK::Args] args
 def tick(args)
+  $trace_service.reset_tick
+  $trace_service.mark('tick')
   t                      = Time.now.usec
   dt                     = t - $last_time
   dt                     = $moving_avg[$last_idx] if dt < 0
@@ -24,4 +29,17 @@ def tick(args)
   $last_time             = t
   args.state.tps         = ($moving_avg.length * 1000000) / $moving_avg.reduce(:plus)
   Game::tick args
+  $trace_service.mark('tock')
+  $trace_service.finish
+end
+
+def perf_test_soy
+  def Bullets::spawn_new_parametric_bullets(game); return []; end
+  $state.game[:player][:upgrades].push(Upgrade::RANGE_MULT)
+  $state.game[:player][:upgrades].push(Upgrade::RANGE_MULT)
+  $state.game[:player][:upgrades].push(Upgrade::DELAY_MULT)
+  $state.game[:player][:upgrades].push(Upgrade::DELAY_MULT)
+  $state.game[:player][:upgrades].push(Upgrade::DELAY_MULT)
+  $state.game[:player][:upgrades].push(Upgrade::DELAY_MULT)
+  # Now try moving and shooting. With a lot of bullets on screen, the game lags. Then you start moving super fast when you stop firing.
 end
